2dcb12deb8cd5e2660f09eef0aa21de7128d9be4
LucasDCoelho <darkalp99@gmail.com>
2023-04-05 10:00:19 -0300

    new: add filter

--- Desktop/Lucas_Coelho_FS15/API-fakeStore/script.js
+++ Desktop/Lucas_Coelho_FS15/API-fakeStore/script.js
@@ -1,12 +1,18 @@
+
+let listaDeProdutos = [];
+
+
+
 function buscarCategorias(){
     fetch("https://fakestoreapi.com/products/categories")
     .then(res => res.json())
     .then(dados => {
+        lista = listaDeProdutos;
         dados.forEach(cada => {
             categorias.innerHTML += `   
                 <li class="list-group-item">
                     <h6>
-                        <input type="checkbox">
+                        <input name="${cada.replace(" ", "-")}" type="checkbox" onClick="filtro()">
                         <span>${cada}</span>
                     </h6>
                 </li>`
@@ -21,13 +27,14 @@ function buscarProdutos(){
     .then(dados => {
         dados.forEach(cada => {
             products.innerHTML += `
-            <li>
+            <li class="${cada.category.replace(" ", "-")}">
                 <div class="card">
                     <picture>
                         <img src="${cada.image}" alt="${cada.title}" class="card-img-top">
                     </picture>
                     <div class="card-body">
-                        <h5 class="card-title">${cada.title}</h5>
+                        <h5 class="card-title" title="${cada.title}">${cada.title}</h5>
+                        <div class="badge bg-secondary mb-2">${cada.category}</div>
                         <p class="card-text text-warp">${cada.description}</p>
                         <h6 class="card-text"> $${cada.price} <h6>
                         <a href="" class="btn btn-primary">Adicionar ao Carrinho</a>
@@ -36,4 +43,33 @@ function buscarProdutos(){
             </li>`
         })
     })
-}buscarProdutos()
+}buscarProdutos();
+
+function filtro(){
+    let inputs = document.querySelectorAll("input:checked")
+    let inputs_nomes = [];
+    for(let i = 0; i < inputs.length; i++){
+        inputs_nomes.push(inputs[i].name);
+    }
+
+    console.log(inputs_nomes)
+
+    let lis = document.querySelectorAll("#products li");
+    for(let i = 0; i < lis.length; i++){
+        if(!lis[i].classList.contains(inputs_nomes.toString())){
+            lis[i].style.display = 'none'
+        } else {
+            lis[i].style.display = 'block'
+        }
+        
+        if(0 === inputs_nomes.length){
+            lis[i].style.display = 'block'
+        }
+
+        if( inputs_nomes.length >= 2){
+            if(!lis[i].classList.contains(inputs_nomes.toString())){
+                lis[i].style.display = 'block'
+            }
+        }
+    }
+}
